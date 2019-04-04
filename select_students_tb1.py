dd78b20c94e653b0c79adff578d36b2abe4b2bf0
ramongonze <ramongonze@gmail.com>
2019-04-04 17:12:14 -0300

    Script to remove directories from students I am not taking care

---
+++ select_students_tb1.py
@@ -0,0 +1,50 @@
+# Input: All the files sent to one exercise", from class TB1
+# Output: Remove all directories which are note sudents from id [29,57]
+
+import os
+import shutil
+
+students = {"Luan Silva Tomaz",
+"Lucas Marx Teixeira de Freitas",
+"Lucas Millard Andrade Silva",
+"Lucas Omar Fonseca Ferreira",
+"Luiz Fernando Neves Guimarães",
+"Luiza Guimaraes Raso",
+"Marcos Vinicius Reis Guimaraes Leite",
+"Mariana Nascimento de Souza",
+"Matheus Pangella de Lana",
+"Naiara Aparecida Martins Garcia",
+"Otavio Gabriel Campos",
+"Paulo Henrique Farah Leocadio e Silva",
+"Paulo Igor Rodrigues",
+"Pedro Henrique Bernardes Solha",
+"Pedro Henrique Figueiredo Araujo",
+"Pedro Streit Ferreira",
+"Rafael Expedito de Almeida Rincon",
+"Rafael Goulart Rosse",
+"Rafaela Maria Farnezi Fagundes",
+"Raissa Nepomuceno Freitas",
+"Roberto Henrique Bastos Assis",
+"Thais Ester Rodrigues Costa",
+"Tiago Martires Gregorio",
+"Victor Hugo Augusto Cezario Morais de Oliveira",
+"Victor Hugo da Costa Soares de Oliveira",
+"Vinicius Gabriel Mello Silva",
+"Vitor Barreto Ribeiro",
+"Vitor Maciel Rodrigues",
+"Yasmini Kelly Gomes dos Santos"}
+
+for i in [3,4,5]:
+	os.chdir("/home/ramongonze/Desktop/turma_tb1/exercicio" + str(i))
+
+	for directory in os.listdir():
+		student = directory.split('_')[0]
+
+		if student not in students:
+			shutil.rmtree(directory) # Remove the directory and everything inside it
+		else:
+			os.rename(directory, student)
+
+
+
+
