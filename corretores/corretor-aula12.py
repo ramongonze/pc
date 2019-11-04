24e1c12c45f715cd41cf9d4865d1e3a563ee4bf2
Ramon Gonze <ramongonze@gmail.com>
2019-11-04 15:56:41 -0300

    Finished class 12 answer sheet

---
+++ corretores/corretor-aula12.py
@@ -0,0 +1,164 @@
+"""
+	Entradas (linha de comando):
+		- argv[1] = Caminho completo do arquivo do aluno (um arquivo .py).
+		- argv[2] = Caminho completo do gabarito (um arquivo .py).
+		
+		OBS.: - Deve ser passado somente o nome do arquivo, sem a extensão .py.
+	    Exemplo:
+	    	- Arquivo do aluno: "alunos/joao.py"
+	    	- Gabarito: "gabaritos/aulaX"
+
+		Execução do script:
+		$ python3 corretor-aulaX.py alunos/joao gabaritos/aulaX
+
+	Saída (stdout): 
+		1ª linha: String contando o caractere '0' se o aluno não obteve 0 em nenhuma questão ou os 
+				  números dos exercícios que ele obteve zero, por exemplo: '1.1,2,3,4.4'
+		2ª linha: Nota final do aluno para o exercício, no intervalo [0,1] (porcentagem).
+		   		  Em caso de erro, a saída será uma string com o erro correspondente.
+"""
+
+import sys, signal
+from random import randint
+from random import seed
+seed(7)
+
+def signalHandler(signum, frame):
+    raise Exception('Timed out!')
+
+################################# Definições ######################################################
+signal.signal(signal.SIGALRM, signalHandler)
+tempoAlarme = 1 # Quantidade de segundos para executar 1 exercício
+notaFinal = 0.0
+
+"""
+	A variável 'exercicios' deve ser uma lista de strings com os nomes das funções dos exercícios.
+	Ex.: exercicios = ["exercicio_1_1", "exercicio_1_2", "exercicio_2", ...]
+"""
+exercicios = [\
+			 ]
+
+numExercicios = len(exercicios) # Número TOTAL de exercícios
+
+"""
+	A variável 'testes' deve ser uma lista onde cada elemento é uma lista com testes.
+	--> testes[i] deve conter uma lista de testes correspondente à exercicios[i].
+	Ex.: testes = [[1,2,3,4,5], [(0,1), (0,2), (0,4)], [x for x in range(10)], ...]
+
+	--> O teste em si são os argumentos que a função irá receber. Caso a função do exercício receba
+		mais que um parâmetro, então o elemento será uma tupla.
+
+	--> Se a função não receber nenhum parâmetro, a sua lista de testes deverá ser uma lista vazia.
+"""
+testes = [\
+		 ]
+
+# Armazena quais exercícios o aluno alcançou nota 0 (zero)
+erros = ''
+
+####################################################################################################
+
+# Try-catch usado para verificar se há erro de sintaxe no arquivo do aluno
+try:
+	aluno = __import__(sys.argv[1])
+except:
+	print('Erro ao importar o arquivo do aluno.')
+
+# Try-catch usado para verificar se há erro de sintaxe no arquivo do aluno
+try:
+	gabarito = __import__(sys.argv[2])
+except:
+	print('Erro ao importar o gabarito.')
+
+
+############################################# Correção #############################################
+for i in range(numExercicios):
+	nTestes = len(testes[i]) # Números de testes para o i-ésimo exercício
+
+	notaParcial = 0.0
+	if nTestes == 0:
+		# A função não recebe parâmetros
+
+		execucaoGabarito = 'saidaGabarito = gabarito.' + exercicios[i] + '()'
+		exec(execucaoGabarito)
+		
+		try:
+			execucaoAluno = 'saidaAluno = aluno.' + exercicios[i] + '()'
+			signal.alarm(tempoAlarme) # Ativa o alarme
+			exec(execucaoAluno)
+			signal.alarm(0) # Cancela o alarme
+
+			assert(saidaGabarito == saidaAluno)
+			notaParcial = 1
+		except Exception:
+			None
+	else:
+		# A Função possui pelo menos 1 parâmetro
+
+		for j in range(nTestes):
+			if type(testes[i][j]) != type((0,0)):
+				# A função possui somente 1 parâmetro
+				execucaoGabarito = 'saidaGabarito = gabarito.' + exercicios[i] + '(' + str(testes[i][j]) + ')'
+				exec(execucaoGabarito)
+				
+				try:
+					execucaoAluno = 'saidaAluno = aluno.' + exercicios[i] + '(' + str(testes[i][j]) + ')'
+					signal.alarm(tempoAlarme) # Ativa o alarme
+					exec(execucaoAluno)
+					signal.alarm(0) # Cancela o alarme
+
+					assert(saidaGabarito == saidaAluno)
+					notaParcial += (1/nTestes)
+				except Exception:
+					continue
+			else:
+				# A função possui mais de 1 parâmetro
+				nParam = len(testes[i][j]) # Número de parâmetros da função
+
+				execucaoGabarito = 'saidaGabarito = gabarito.' + exercicios[i] + '('
+				execucaoAluno = 'saidaAluno = aluno.' + exercicios[i] + '('
+				for parametro in range(nParam):
+					execucaoGabarito = execucaoGabarito + str(testes[i][j][parametro]) + ','
+					execucaoAluno 	 = execucaoAluno    + str(testes[i][j][parametro]) + ','
+
+				execucaoGabarito = execucaoGabarito[:-1] + ')' # Substitui a última vírgula por ')'
+				execucaoAluno 	 = execucaoAluno[:-1] + ')' # Substitui a última vírgula por ')'
+
+				exec(execucaoGabarito)
+
+				try:
+					signal.alarm(tempoAlarme) # Ativa o alarme
+					exec(execucaoAluno)
+					signal.alarm(0) # Cancela o alarme
+
+					assert(saidaGabarito == saidaAluno)
+					notaParcial += (1/nTestes)
+				except Exception:
+					continue
+
+	# Seleciona o número do exercício e adiciona em erros
+	if notaParcial == 0:
+		tokens = exercicios[i].split('_')
+		ex = ''
+		for j in range(1,len(tokens)):
+			ex = ex + tokens[j] + '.'
+		ex = ex[:-1]
+		erros = erros + ex + ','
+	else:
+		notaFinal += (notaParcial * 1.0/numExercicios)
+####################################################################################################
+
+# Se a nota do aluno for >= 0.99, arredonda para 1.0, para o caso de o aluno ter acertado todas 
+# as questões e haver um erro de precisão no somatório das notas parciais.
+if notaFinal >= 0.99:
+	notaFinal = 1.0
+
+if erros == '':
+	# O aluno não obteve nota zero em nenhum exercício
+	print('0', end='#')
+else:
+	print(erros[:-1], end='#')
+
+# Imprime a nota final do aluno com 4 casas decimais.
+# A nota está no intervalo [0,1]
+print('%.2f'%notaFinal)
--- gabaritos/gabarito-aula12.py
+++ gabaritos/gabarito-aula12.py
@@ -114,8 +114,23 @@ def exercicio_12_3_3(representacao, letra, codigo):
 		return None
 	return representacao
 
-def exercicio_12_3_4(string, lista_de_contagens):
+def exercicio_12_3_4(simbolos, representacao):
+	ans = []
 
+	if type(representacao) == type((0,0)):
+		for s in simbolos:
+			if s in representacao[0]:
+				ans.append((s,representacao[1][representacao[0].index(s)]))
+	elif type(representacao[0]) == type('a'):
+		for s in simbolos:
+			if s in representacao:
+				ans.append((s,representacao[representacao.index(s)+1]))
+	else:
+		for letra, count in representacao:
+			if letra in simbolos:
+				ans.append((s,count))
+
+	return sorted(ans)
 
 def exercicio_12_3_5(representacao, letras):
 	if type(representacao) == type((0,0)):
@@ -146,4 +161,180 @@ def exercicio_12_3_5(representacao, letras):
 	return representacao
 
 def exercicio_12_3_6(representacao, letras):
+	ans = []
+
+	if type(representacao) == type((0,0)):
+		for s in simbolos:
+			if s in representacao[0]:
+				ans.append((s,representacao[1][representacao[0].index(s)]))
+	elif type(representacao[0]) == type('a'):
+		for s in simbolos:
+			if s in representacao:
+				ans.append((s,representacao[representacao.index(s)+1]))
+	else:
+		for letra, count in representacao:
+			if letra in simbolos:
+				ans.append((s,count))
+
+	if ans == []:
+		return None
+
+	return sorted(ans)
+
+def exercicio_12_4_1(string):
+	vogais = 'aeiou'
+	ans = [[],[]]
+	d = {}
+	for letra in string:
+		if letra in d:
+			d[letra] += 1
+		else:
+			d[letra] = 1
+	for letra in d:
+		if letra in vogais:
+			ans[0].append((letra, string.count(letra)))
+		else:
+			ans[1].append((letra, string.count(letra)))
+	ans[0] = sorted(ans[0])
+	ans[1] = sorted(ans[1])
+	return tuple(ans)
+
+def exercicio_12_4_2(dicionario,letra):
+	if letra not in dicionario:
+		return None
+	else:
+		return dicionario[letra]	
+
+def exercicio_12_4_3(dicionario,letra,codigo):
+	if letra not in dicionario:
+		return None
+
+	if codigo == 0:
+		dicionario[letra] = 0
+	else codigo == -1:
+		del dicionario[letra]
+	else:
+		return None
+
+	return dicionario
+
+
+def exercicio_12_4_4(string,dicionario):
+	ans = {}
+	for letra in string:
+		if letra in dicionario:
+			ans[letra] = dicionario[letra]
+
+	return ans
+
+def exercicio_12_4_5(dicionario,letras):
+	for l in letras:
+		if l not in dicionario:
+			dicionario[l] = 0
+		else:
+			dicionario[l] += 1
+	return dicionario
+
+def exercicio_12_4_6(dicionario,letras):
+	noLetter = False
+
+	for l in letras:
+		if l in dicionario:
+			dicionario[l] += 1
+			noLetter = True
+
+	if not noLetter:
+		return None
+	else:
+		return dicionario
+
+def exercicio_12_5_1(agenda, nome, numero):
+	if nome == '' or numero == '':
+		return agenda
+
+	agenda[nome].append(numero)
+
+	return agenda
+
+def exercicio_12_5_2(agenda, nome, numero):
+	if nome == '' and numero == '':
+		return None
+	elif nome == '' and numero != '':
+		count = 0
+		for pessoa in agenda:
+			if numero in agenda[pessoa]:
+				agenda[pessoa].remove(numero)
+				if agenda[pessoa] == []:
+					del agenda[pessoa]
+				count += 1
+		
+		if count == 0:
+			return None
+
+	elif nome != '' and numero == '':
+		if nome in agenda:
+			agenda[nome] = []
+		else:
+			return None
+	else:
+		if nome in agenda and numero in agenda[nome]:
+			agenda[nome].remove(numero)
+		else:
+			return None
+
+	return agenda
+
+def exercicio_12_5_3(agenda, nome1, numero1, nome2, numero2):
+	if nome1 != '' and numero1 != '' and nome2 != '' and numero2 != '':
+		agenda[nome1].remove(numero1)
+		agenda[nome1].append(numero2)
+		agenda[nome2] = agenda[nome1]
+		del agenda[nome1]
+
+	elif nome1 != '' and numero1 == '' and nome2 != '' and numero2 == '':
+		agenda[nome2] = agenda[nome1]
+		del agenda[nome1]
+
+	elif nome1 != '' and numero1 != '' and nome2 == '' and numero2 != '':
+		agenda[nome1].remove(numero1)
+		agenda[nome2].append(numero2)
+
+	elif nome1 != '' and numero1 == '' and nome2 != '' and numero2 != '':
+		del agenda[nome1]
+		agenda[nome2] = [numero2]
+
+	elif nome1 == '' and numero1 != '' and nome2 == '' and numero2 != '':
+		for pessoa in agenda:
+			if numero1 in agenda[pessoa]:
+				agenda[pessoa].remove(numero1)
+				agenda[pessoa].append(numero2)
+
+	elif nome1 == '' and numero1 != '' and nome2 != '' and numero2 != '':
+		agenda[nome2] = [numero1, numero2]
+
+	else:
+		return None
+
+	return agenda
+
+def exercicio_12_5_4(agenda):
+	ans = ''
+
+	for pessoa in agenda:
+		ans = pessoa + ': '
+		for telefone in agenda[pessoa]:
+			ans = ans + telefone + '; '
+		ans += '\n'
 	
+	return ans
+
+def exercicio_12_5_5(agenda, nome):
+	if nome not in agenda:
+		return None
+
+	ans = nome + ': '
+	for telefone in agenda[nome]:
+		ans = ans + telefone + '; '
+	ans += '\n'
+
+	return ans
